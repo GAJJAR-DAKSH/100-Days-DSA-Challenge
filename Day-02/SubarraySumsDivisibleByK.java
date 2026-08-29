a518fe16fd31634aca536237896d489b351b52b6
GAJJAR DAKSH <dakshgajjar0@gmail.com>
2026-08-29 09:44:36 +0530

    Add solution for subarray sums divisible by K
    Implement a solution to count subarrays with sums divisible by k.

---
+++ Day-02/SubarraySumsDivisibleByK.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // remainder 0 exists before starting
+        map.put(0, 1);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            sum += num;
+
+            int rem = ((sum % k) + k) % k;
+
+            if (map.containsKey(rem)) {
+                count += map.get(rem);
+            }
+
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
+        }
+
+        return count;
+    }
+}
